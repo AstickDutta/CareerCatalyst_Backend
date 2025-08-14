e7b571d1ee2db34695ad772206b1424878429bc1
deeksha <gdeekshi22@gmail.com>
2025-08-14 09:53:38 +0530

    added fields commit

--- app/models/Applications_model.py
+++ app/models/Applications_model.py
@@ -17,3 +17,6 @@ class Application(db.Model):
     applicant_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     resume_url = db.Column(db.String(255), nullable=False)
     status = db.Column(Enum(ApplicationStatus), nullable=False, default=ApplicationStatus.applied)
+    applied_at=db.Column(db.DateTime, default=datetime.utcnow)
+    created_at=db.Column(db.DateTime)
+    updated_at=db.Column(db.DateTime)
